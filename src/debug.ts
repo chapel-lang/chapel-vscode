e908a7a9e8dfe203c05178abf68418ffc6139633
Jade Abraham <jade.abraham@hpe.com>
2025-04-17 08:57:00 -0700

    refactor commands and add command for current file
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- src/debug.ts
+++ src/debug.ts
@@ -24,56 +24,113 @@ import { getPreferredDebugProvider } from './configuration';
 export function registerDebugCommands(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerCommand(
-      "chapel.generateGenericDebug",
-      async () => {
-        // determine the provider
-        const provider = getPreferredProvider();
-        if (!provider) {
-          const preferredProviderId = getPreferredDebugProvider();
-          vscode.window.showErrorMessage(`The requested debug provider ${preferredProviderId} is not installed/supported`);
-          return;
-        }
-
-        if (!provider.isAvailable()) {
-          vscode.window.showErrorMessage(`The requested debug provider ${provider.id} is not installed. Please install [${provider.name}](command:${provider.getInstallCommand()}) or select another provider.`);
-          return;
-        }
-
-        // TODO: we could do some kind of detection for mason projects, maybe even Make/CMake?
-        // TODO: for the executables, we could search for all executable files?
-        // TODO create a custom webview to let the user select the executable and env vars and args
-        // for now, prompt the user for a name and a path the executable
-
-        const name = await vscode.window.showInputBox({ placeHolder: "Enter the name of the debug configuration" });
-        if (!name) {
-          return;
-        }
-        const executable = await vscode.window.showInputBox({ placeHolder: "Enter the path to the executable" });
-        if (!executable) {
-          return;
-        }
-
-        const newConfig = provider.createDebugConfig(name, executable);
-
-        const launchConfigs = vscode.workspace.getConfiguration("launch");
-        const configs = launchConfigs.get<any[]>("configurations") || [];
-        const existingConfigIdx = configs.findIndex((config: any) => config.name === name);
-        if (existingConfigIdx !== -1) {
-          const choice = await vscode.window.showErrorMessage(`A debug configuration with the name ${name} already exists.`, "Cancel", "Overwrite");
-          if (choice !== "Cancel") {
-            configs[existingConfigIdx] = newConfig;
-          }
-        } else {
-          configs.push(newConfig);
-        }
-        launchConfigs.update("configurations", configs).then(() => {
-          vscode.window.showInformationMessage(`Debug configuration ${name} created successfully.`);
-        }, (err) => {
-          vscode.window.showErrorMessage(`Failed to create debug configuration: ${err}`);
-        });
-      }
+      "chapel.createDebugConfig",
+      chapel_createDebugConfig
     )
   );
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "chapel.createDebugConfigForActiveFile",
+      chapel_createDebugConfigForActiveFile
+    )
+  );
+}
+async function chapel_createDebugConfig() {
+  const provider = getProvider();
+  if (!provider) {
+    return
+  }
+  // TODO: we could do some kind of detection for mason projects, maybe even Make/CMake?
+  // TODO: for the executables, we could search for all executable files?
+  // TODO create a custom webview to let the user select the executable and env vars and args
+  // for now, prompt the user for a name and a path the executable
+
+  const name = await vscode.window.showInputBox({ placeHolder: "Enter the name of the debug configuration" });
+  if (!name) {
+    return;
+  }
+  const executable = await vscode.window.showInputBox({ placeHolder: "Enter the path to the executable" });
+  if (!executable) {
+    return;
+  }
+
+  const newConfig = provider.createDebugConfig(name, executable);
+  await saveConfig(newConfig);
+}
+
+async function chapel_createDebugConfigForActiveFile() {
+  const provider = getProvider();
+  if (!provider) {
+    return
+  }
+
+  // get the current file
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    vscode.window.showErrorMessage("No active editor found.");
+    return;
+  }
+  const document = editor.document;
+  if (document.languageId !== "chapel") {
+    vscode.window.showErrorMessage("The current file is not a Chapel file.");
+    return;
+  }
+  const executablePath = document.fileName?.split(".").slice(0, -1).join(".");
+  if (!executablePath) {
+    vscode.window.showErrorMessage("Failed to get the file name.");
+    return;
+  }
+  const wsPath = vscode.workspace.workspaceFolders?.[0]?.uri.fsPath;
+  if (!wsPath) {
+    vscode.window.showErrorMessage("No workspace folder found.");
+  }
+  let executableNiceName: string = executablePath.split("/").pop() || executablePath;
+  let executableRelPath: string = executablePath;
+  if (wsPath) {
+    // determine executableRelPath to wsPath
+    executableNiceName = executablePath.replace(wsPath + "/", "");
+    executableRelPath = "${workspaceFolder}/" + executableNiceName;
+  }
+
+  const newConfig = provider.createDebugConfig(`Debug ${executableNiceName}`, executableRelPath);
+  await saveConfig(newConfig);
+}
+
+
+function getProvider(): DebugProvider | undefined {
+  // determine the provider
+  const provider = getPreferredProvider();
+  if (!provider) {
+    const preferredProviderId = getPreferredDebugProvider();
+    vscode.window.showErrorMessage(`The requested debug provider '${preferredProviderId}' is not installed/supported`);
+    return;
+  }
+
+  if (!provider.isAvailable()) {
+    vscode.window.showErrorMessage(`The requested debug provider '${provider.id}' is not installed. Please install [${provider.name}](command:${provider.getInstallCommand()}) or select another provider.`);
+    return;
+  }
+  return provider;
+}
+
+async function saveConfig(newConfig: vscode.DebugConfiguration) {
+  const name = newConfig.name;
+  const launchConfigs = vscode.workspace.getConfiguration("launch");
+  const configs = launchConfigs.get<any[]>("configurations") || [];
+  const existingConfigIdx = configs.findIndex((config: any) => config.name === name);
+  if (existingConfigIdx !== -1) {
+    const choice = await vscode.window.showErrorMessage(`A debug configuration named '${name}' already exists.`, "Cancel", "Overwrite");
+    if (choice !== "Cancel") {
+      configs[existingConfigIdx] = newConfig;
+    }
+  } else {
+    configs.push(newConfig);
+  }
+  launchConfigs.update("configurations", configs).then(() => {
+    vscode.window.showInformationMessage(`Debug configuration '${name}' created successfully.`);
+  }, (err) => {
+    vscode.window.showErrorMessage(`Failed to create debug configuration: ${err}`);
+  });
 }
 
 class DebugProvider {
