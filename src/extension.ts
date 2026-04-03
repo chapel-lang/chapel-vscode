eb4e97dea755485fed34671665c6b1a96bd8db60
Jade Abraham <jade.abraham@hpe.com>
2026-04-02 17:18:55 -0700

    add same debounce to restart on settings
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- src/extension.ts
+++ src/extension.ts
@@ -144,13 +144,20 @@ export function activate(context: vscode.ExtensionContext) {
       }
     )
   );
+  let configChangeTimeout: ReturnType<typeof setTimeout> | undefined;
   context.subscriptions.push(
     vscode.workspace.onDidChangeConfiguration(async (e) => {
       if (e.affectsConfiguration("chapel")) {
-        Promise.all([
-          chplcheckClient.resetConfig(getChplCheckConfig()),
-          clsClient.resetConfig(getCLSConfig()),
-        ]);
+        if (configChangeTimeout !== undefined) {
+          clearTimeout(configChangeTimeout);
+        }
+        configChangeTimeout = setTimeout(async () => {
+          configChangeTimeout = undefined;
+          Promise.all([
+            chplcheckClient.resetConfig(getChplCheckConfig()),
+            clsClient.resetConfig(getCLSConfig()),
+          ]);
+        }, 5000);
       }
     })
   );
